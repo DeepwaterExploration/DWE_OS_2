86ba20d258d3ad40b6a7105224fbc905170164ba
brandonhs <brandonnms@gmail.com>
2023-06-26 10:55:14 -0700

    remove build files

--- backend/build/thirdparty/cwalk/CMakeFiles/cwalk.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cwalk.
-# This may be replaced when dependencies are built.
--- backend/build/thirdparty/cwalk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kunal/Documents/GitHub/explorehd-test-driver/libehd/thirdparty/cwalk
-# Build directory: /home/kunal/Documents/GitHub/explorehd-test-driver/libehd/build/thirdparty/cwalk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
