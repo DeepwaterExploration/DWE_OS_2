8819d0794024792cf0fa293fda342a158c9ae28e
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2023-06-27 14:27:00 -0700

    Fixed building issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,11 @@ import { block } from 'million/react';
 import './App.css';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <div className="App">
+      {/* <Dashboard />; */}
+    </div>
+  )
 }
 
 const AppBlock = block(App);
