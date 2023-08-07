0ec9c6ca56f4370ff349350cbfb2637cb64ef3c9
brandonhs <brandonnms@gmail.com>
2023-08-07 12:40:41 -0700

    changes to build process

--- build_repo.sh
+++ build_repo.sh
@@ -1,11 +1,11 @@
 # Go to frontend folder
 cd frontend
-./build_frontend.sh
+sh ./build_frontend.sh
 # Return to root folder
 cd ..
 
 # Go to backend folder
 cd backend
-./build_backend.sh
+sh ./build_backend.sh
 # Return to root folder
 cd ..
