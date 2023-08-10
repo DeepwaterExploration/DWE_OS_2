7a6e97d5794b786573d1ebaf00e51acea3beaef0
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-08-10 12:12:59 -0700

    Create socket directory if it does not previously exist

--- system_api/wifi/wifiHandler.py
+++ system_api/wifi/wifiHandler.py
@@ -1,5 +1,7 @@
 from typing import List
+from pathlib import Path
 import subprocess, os, select, socket
+import glob
 
 def find_valid_interfaces() -> List[str]:
     """Returns a list of valid network interfaces."""
@@ -17,8 +19,16 @@ def find_valid_interfaces() -> List[str]:
 wpa_send_path = f"/run/wpa_supplicant/{find_valid_interfaces()[0]}"
 wpa_recv_path = "/tmp/wpa_ctrl_{pid}-{count}".format(pid=os.getpid(), count=1)
 
+Path(wpa_recv_path).mkdir(parents=True, exist_ok=True)
+
 soc = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM, 0)
-soc.bind(wpa_recv_path)
+# clear path
+files = glob.glob(f"{wpa_recv_path}/*")
+for f in files:
+    os.remove(f)
+socket_client = f"{wpa_recv_path}/wpa_supplicant_service_{os.getpid()}"
+soc.bind(socket_client)
+soc.settimeout(10)
 soc.connect(wpa_send_path)
 
 print("> PING")
