c4f16c7684b05e67be2bafad5b51c348902120ea
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-08-10 12:36:07 -0700

    Add wpa supplicent support

--- system_api/wifi/wifiHandler.py
+++ system_api/wifi/wifiHandler.py
@@ -1,4 +1,5 @@
-from typing import List
+import asyncio
+from typing import List, Optional, Tuple, Union
 from pathlib import Path
 import subprocess, os, select, socket
 import glob
@@ -16,21 +17,54 @@ def find_valid_interfaces() -> List[str]:
     except subprocess.CalledProcessError:
         return []
 
-wpa_send_path = f"/run/wpa_supplicant/{find_valid_interfaces()[0]}"
-wpa_recv_path = "/tmp/wpa_ctrl_{pid}-{count}".format(pid=os.getpid(), count=1)
+class WPASupplicant:
+    """Represents a wrapper around wpa_supplicant, which is used to manage WiFi connections."""
+    BUFFER_SIZE = 4096
+    DEFAULT_TARGET: Union[Tuple[str, int], str] = ("localhost", 4200) # Default target for socket communication. Either a tuple of (IP address, port) or a path to a UNIX socket.
+    TIMEOUT_LIMIT = 10 # Timeout for socket communication and scanning for WiFi networks
+    SOCKET_SEND_PATH = f"/run/wpa_supplicant/{find_valid_interfaces()[0]}" # Path to the UNIX socket used for communication with wpa_supplicant
+    SOCKET_RECV_PATH = f"/tmp/wpa_ctrl_{os.getpid()}-1" # Path to the UNIX socket used for receiving data from wpa_supplicant
 
-Path(wpa_recv_path).mkdir(parents=True, exist_ok=True)
+    def __init__(self) -> None:
+        """Initializes the WPASupplicant object."""
+        self.sock: Optional[socket.socket] = None
 
-soc = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM, 0)
-# clear path
-files = glob.glob(f"{wpa_recv_path}/*")
-for f in files:
-    os.remove(f)
-socket_client = f"{wpa_recv_path}/wpa_supplicant_service_{os.getpid()}"
-soc.bind(socket_client)
-soc.settimeout(10)
-soc.connect(wpa_send_path)
+    def __del__(self) -> None:
+        """Closes the socket connection when the object is destroyed."""
+        if self.sock:
+            self.sock.close()
+    
+    def run(self, target: Union[Tuple[str, int], str] = DEFAULT_TARGET) -> None:
+        """Does the connection and setup variables
 
-print("> PING")
-soc.send(b"PING")
-print("<", soc.recv(4096).decode().strip())
+        Arguments:
+            path {[tuple/str]} -- Can be a tuple to connect (ip/port) or unix socket file
+        """
+        Path(self.SOCKET_RECV_PATH).mkdir(parents=True, exist_ok=True)
+
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM, 0)
+
+        # clear path
+        files = glob.glob(f"{self.SOCKET_RECV_PATH}/*")
+        for file in files:
+            os.remove(file)
+        socket_client = f"{self.SOCKET_RECV_PATH}/wpa_supplicant_service_{os.getpid()}"
+        
+        print(f"Connecting to {self.SOCKET_SEND_PATH}")
+        # 
+        self.sock.settimeout(10)
+        # Bind the receiving socket
+        self.sock.bind(socket_client)
+        # Connect to the sending socket
+        self.sock.connect(self.SOCKET_SEND_PATH)
+
+        print("> PING")
+        self.sock.send(b"PING")
+        print("<", self.sock.recv(4096).decode().strip())
+
+async def main() -> None:
+    wpa = WPASupplicant()
+    wpa.run(("localhost", 6664))
+
+if __name__ == "__main__":
+    asyncio.run(main())
