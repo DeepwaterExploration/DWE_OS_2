f1848e3e135b81aca3e9acf78aaca6846aff6e52
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-08-10 15:58:47 -0700

    Update wpa_supplicant.py

--- system_api/wifi/wpa_supplicant.py
+++ system_api/wifi/wpa_supplicant.py
@@ -4,14 +4,37 @@ import os
 import socket
 import time
 from pathlib import Path
-from typing import Optional, Tuple, Union
+from typing import List, Optional, Tuple, Union
+import subprocess
 
-from exceptions import BusyError, NetworkAddFail, SockCommError, WPAOperationFail
+
+def find_valid_interfaces() -> List[str]:
+    """Returns a list of valid network interfaces."""
+    try:
+        return (
+            subprocess.check_output(
+                "iwconfig 2>/dev/null | grep -o '^[[:alnum:]]*'", shell=True
+            )
+            .decode()
+            .split()
+        )
+    except subprocess.CalledProcessError:
+        return []
+
 
 class WPASupplicant:
     """Represents a wrapper around wpa_supplicant, which is used to manage WiFi connections."""
-    BUFFER_SIZE = 4096 # Max buffer size for receiving data
-    DEFAULT_TARGET: Union[Tuple[str, int], str] = ("localhost", 6664) # Default target for socket communication. Either a tuple of (IP address, port) or a path to a UNIX socket.
+
+    BUFFER_SIZE = 4096
+    DEFAULT_TARGET: Union[Tuple[str, int], str] = (
+        "localhost",
+        4200,
+    )  # Default target for socket communication. Either a tuple of (IP address, port) or a path to a UNIX socket.
+    TIMEOUT_LIMIT = (
+        10  # Timeout for socket communication and scanning for WiFi networks
+    )
+    SOCKET_SEND_PATH = f"/run/wpa_supplicant/{find_valid_interfaces()[0]}"  # Path to the UNIX socket used for communication with wpa_supplicant
+    SOCKET_RECV_PATH = f"/tmp/wpa_ctrl_{os.getpid()}-1"  # Path to the UNIX socket used for receiving data from wpa_supplicant
 
     def __init__(self) -> None:
         """Initializes the WPASupplicant object."""
@@ -28,25 +51,23 @@ class WPASupplicant:
         Arguments:
             path {[tuple/str]} -- Can be a tuple to connect (ip/port) or unix socket file
         """
-        self.target = target
+        Path(self.SOCKET_RECV_PATH).mkdir(parents=True, exist_ok=True)
 
-        wpa_playground_path = "/tmp/wpa_playground"
-        Path(wpa_playground_path).mkdir(parents=True, exist_ok=True)
+        self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM, 0)
 
-        if isinstance(self.target, tuple):
-            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
-            # clear path
-            files = glob.glob(f"{wpa_playground_path}/*")
-            for f in files:
-                os.remove(f)
-            socket_client = f"{wpa_playground_path}/wpa_supplicant_service_{os.getpid()}"
-            self.sock.bind(socket_client)
+        # clear path
+        files = glob.glob(f"{self.SOCKET_RECV_PATH}/*")
+        for file in files:
+            os.remove(file)
+        socket_client = f"{self.SOCKET_RECV_PATH}/wpa_supplicant_service_{os.getpid()}"
 
+        print(f"Connecting to {self.SOCKET_SEND_PATH}")
+        #
         self.sock.settimeout(10)
-        self.sock.connect(self.target)
-
+        # Bind the receiving socket
+        self.sock.bind(socket_client)
+        # Connect to the sending socket
+        self.sock.connect(self.SOCKET_SEND_PATH)
 
     async def send_command(self, command: str, timeout: float) -> bytes:
         """Send a specific WPA Supplicant command. Raises an exception if the command fails to answer before the specified timeout.
@@ -63,7 +84,9 @@ class WPASupplicant:
                 self.sock.send(command.encode("utf-8"))
                 data, _ = self.sock.recvfrom(self.BUFFER_SIZE)
             except Exception as error:
-                raise SockCommError("Could not communicate with WPA Supplicant socket.") from error
+                raise SockCommError(
+                    "Could not communicate with WPA Supplicant socket."
+                ) from error
 
             if b"FAIL-BUSY" in data:
                 # logger.info(f"Busy during {command} operation. Trying again...")
@@ -71,12 +94,24 @@ class WPASupplicant:
                 continue
             break
         else:
-            raise BusyError(f"{command} operation took more than specified timeout ({timeout}). Cancelling.")
+            raise BusyError(
+                f"{command} operation took more than specified timeout ({timeout}). Cancelling."
+            )
 
         if data == b"FAIL":
             raise WPAOperationFail(f"WPA operation {command} failed.")
 
         return data
+
+    async def send_command_ping(self, timeout: float = 1) -> bytes:
+        """Send message: PING
+
+        This command can be used to test whether wpa_supplicant is replying to
+            the control interface commands. The expected reply is  <code>PONG</code>
+            if the connection is open and wpa_supplicant is processing commands.
+
+        """
+        return await self.send_command("PING", timeout)
 
     async def send_command_mib(self, timeout: float = 1) -> bytes:
         """Send message: MIB
@@ -87,6 +122,13 @@ class WPASupplicant:
         """
         return await self.send_command("MIB", timeout)
 
+    async def send_command_reconfigure(self, timeout: float = 1) -> bytes:
+        """Send message: RECONFIGURE
+
+        Force wpa_supplicant to re-read its configuration data.
+        """
+        return await self.send_command("RECONFIGURE", timeout)
+
     async def send_command_status(self, timeout: float = 1) -> bytes:
         """Send message: STATUS
 
@@ -96,7 +138,9 @@ class WPASupplicant:
         """
         return await self.send_command("STATUS", timeout)
 
-    async def send_command_set(self, variable: str, value: str, timeout: float = 1) -> bytes:
+    async def send_command_set(
+        self, variable: str, value: str, timeout: float = 1
+    ) -> bytes:
         """Send message: SET
 
         Example command:
@@ -132,7 +176,9 @@ class WPASupplicant:
         """
         return await self.send_command("SCAN_RESULTS", timeout)
 
-    async def send_command_select_network(self, network_id: int, timeout: float = 1) -> bytes:
+    async def send_command_select_network(
+        self, network_id: int, timeout: float = 1
+    ) -> bytes:
         """Send message: SELECT_NETWORK
 
         Select a network (disable others). Network id can be received from the
@@ -140,7 +186,9 @@ class WPASupplicant:
         """
         return await self.send_command(f"SELECT_NETWORK {network_id}", timeout)
 
-    async def send_command_enable_network(self, network_id: int, timeout: float = 1) -> bytes:
+    async def send_command_enable_network(
+        self, network_id: int, timeout: float = 1
+    ) -> bytes:
         """Send message: ENABLE_NETWORK
 
         Enable a network. Network id can be received from the  <code>LIST_NETWORKS</code>
@@ -149,7 +197,9 @@ class WPASupplicant:
         """
         return await self.send_command(f"ENABLE_NETWORK {network_id}", timeout)
 
-    async def send_command_disable_network(self, network_id: int, timeout: float = 1) -> bytes:
+    async def send_command_disable_network(
+        self, network_id: int, timeout: float = 1
+    ) -> bytes:
         """Send message: DISABLE_NETWORK
 
         Disable a network. Network id can be received from the  <code>LIST_NETWORKS</code>
@@ -172,7 +222,9 @@ class WPASupplicant:
             raise NetworkAddFail("Add_network operation did not return a valid id.")
         return int(network_id.strip())
 
-    async def send_command_remove_network(self, network_id: int, timeout: float = 1) -> bytes:
+    async def send_command_remove_network(
+        self, network_id: int, timeout: float = 1
+    ) -> bytes:
         """Send message: REMOVE_NETWORK
 
         Remove a network. Network id can be received from the  <code>LIST_NETWORKS</code>
@@ -181,15 +233,21 @@ class WPASupplicant:
         """
         return await self.send_command(f"REMOVE_NETWORK {network_id}", timeout)
 
-    async def send_command_set_network(self, network_id: int, variable: str, value: str, timeout: float = 1) -> bytes:
+    async def send_command_set_network(
+        self, network_id: int, variable: str, value: str, timeout: float = 1
+    ) -> bytes:
         """Send message: SET_NETWORK
 
         This command uses the same variables and data formats as the configuration
             file. See example wpa_supplicant.conf for more details.
         """
-        return await self.send_command(f"SET_NETWORK {network_id} {variable} {value}", timeout)
+        return await self.send_command(
+            f"SET_NETWORK {network_id} {variable} {value}", timeout
+        )
 
-    async def send_command_get_network(self, network_id: int, variable: str, timeout: float = 1) -> bytes:
+    async def send_command_get_network(
+        self, network_id: int, variable: str, timeout: float = 1
+    ) -> bytes:
         """Send message: GET_NETWORK
 
         Get network variables. Network id can be received from the  <code>LIST_NETWORKS</code>
@@ -204,16 +262,17 @@ class WPASupplicant:
         """
         return await self.send_command("SAVE_CONFIG", timeout)
 
+
 async def main() -> None:
     wpa = WPASupplicant()
-    wpa.run(("localhost", 6664))
+    wpa.run(("localhost", 7778))
     time.sleep(1)
     await wpa.send_command_list_networks()
     for i in range(5):
         await wpa.send_command_remove_network(i)
 
     await wpa.send_command_add_network()
-    await wpa.send_command_set_network(0, "ssid", '"wifi_ssid"')
+    await wpa.send_command_set_network(0, "EvoNexusSD", '"S@nD1ego!"')
     await wpa.send_command_set_network(0, "psk", '"wifi_password"')
     await wpa.send_command_enable_network(0)
     await wpa.send_command_save_config()
