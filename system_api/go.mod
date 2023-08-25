d20e33926a35fc0b4ee1970f859724e956a729c1
Miguel Villa Floran <miguel.villafloran@gmail.com>
2023-08-25 03:09:48 -0700

    Add api endpoints

--- system_api/go.mod
+++ system_api/go.mod
@@ -3,6 +3,9 @@ module system_api
 go 1.18
 
 require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
--- system_api/main.go
+++ system_api/main.go
@@ -3,14 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
+
+	// Initialize the Wi-Fi handler
+	// wifiHandler = NewWifiHandler()
+
+	r := mux.NewRouter()
+	// r.HandleFunc("/wifiStatus", handleWifiStatus).Methods("GET")
+	// r.HandleFunc("/wifiConnected", handleWifiConnected).Methods("GET")
+	// r.HandleFunc("/wifiScan", handleWifiScan).Methods("GET")
+	// r.HandleFunc("/wifiSaved", handleWifiSaved).Methods("GET")
+	// r.HandleFunc("/wifiToggle", toggleWifiStatus).Methods("POST")
+	// r.HandleFunc("/wifiConnect", connectToWifi).Methods("POST")
+	// r.HandleFunc("/wifiForget", forgetWifi).Methods("POST")
+	// r.HandleFunc("/wifiDisconnect", disconnectFromWifi).Methods("POST")
+	// r.HandleFunc("/shutDownMachine", shutdownMachine).Methods("POST")
+	// r.HandleFunc("/restartMachine", restartMachine).Methods("POST")
+
+	// Create a new HTTP server with the CORS (Cross-Origin Resource Sharing) middleware enabled
+	corsOptions := handlers.CORS(
+		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Accept"}), // Allow only headers from the list
+		handlers.AllowedOrigins([]string{"*"}),                                          // Allow requests from any origin
+		handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}),                     // Allow only GET and POST methods
+	)
+
+	// Set up the server address and port
 	const port = 8080
 	const host = "localhost"
-	http.HandleFunc("/", hello)
-	Log.Info("This is an info message.")
+
+	// Start the server
 	Log.Info(fmt.Sprintf("Server started at http://%s:%d.", host, port))
+	http.Handle("/", corsOptions(r))
 	http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), nil)
 }
 
