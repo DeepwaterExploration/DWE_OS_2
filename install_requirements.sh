898164efc0d81a2c2f58e9ba00edaaeacfd29b46
brandonhs <brandonnms@gmail.com>
2024-01-26 21:29:13 -0800

    changes to requirements to include golang

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo apt install libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
+# install golang
+sudo apt-get install golang
+
+# install gstreamer
+sudo apt-get install libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
+
+# assume python is installed - may not be
