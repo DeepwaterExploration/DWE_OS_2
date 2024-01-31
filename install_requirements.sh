bf3ab8de47883c3a377355528cf72e1bb1030a70
brandonhs <brandonnms@gmail.com>
2024-01-30 19:55:39 -0800

    add python as a requirement

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+# install python
+sudo apt-get install python3
+
 # install golang
 sudo apt-get install golang
 
 # install gstreamer
 sudo apt-get install libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
-
-# assume python is installed - may not be
