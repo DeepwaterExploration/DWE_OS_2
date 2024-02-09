18552ca8b46822f780fecbda04a0665ae0d17510
brandonhs <brandonnms@gmail.com>
2024-02-08 22:29:10 -0800

    update run_release script

--- run_release.sh
+++ run_release.sh
@@ -2,5 +2,9 @@
 
 # expects create_venv.sh to have been run
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+cd ${SCRIPT_DIR}
+
 # needs sudo perms for the go backend
 sudo .env/bin/python3 run_release.py
