7fe82626aae0d625772ee0bd86dbdf67a65fa779
jdszekeres <jdszekeres@gmail.com>
2024-07-01 11:47:30 -0700

    new syspath adder method

--- tests/backend_py/test_recording.py
+++ tests/backend_py/test_recording.py
@@ -1,8 +1,10 @@
 import sys
 import os
 cwd = os.getcwd()
-sys.path.append(cwd)
-os.chdir(os.path.join(cwd,"backend_py/src")) # access device_settings
+src =os.path.join(cwd,"backend_py/src")
+if (os.path.isdir(src)):
+    sys.path.append(cwd)
+    os.chdir(src) # access device_settings
 # import the module from a diffrent path
 
 from backend_py.src import recording
--- tests/backend_py/test_stream.py
+++ tests/backend_py/test_stream.py
@@ -1,7 +1,9 @@
 import sys
 import os
 cwd = os.getcwd()
-sys.path.append(cwd)
-os.chdir(os.path.join(cwd,"backend_py/src")) # access device_settings
+src =os.path.join(cwd,"backend_py/src")
+if (os.path.isdir(src)):
+    sys.path.append(cwd)
+    os.chdir(src) # access device_settings
 # import the module from a diffrent path
 import backend_py.src.stream
