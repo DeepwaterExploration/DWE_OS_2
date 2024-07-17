cee2db856204c75f1f9be732d1c4db7542ab1751
Brandon Stevens <brandonnms@gmail.com>
2024-07-17 12:26:57 -0700

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -7,4 +7,4 @@ sudo apt-get install python3
 sudo apt-get install golang
 
 # install gstreamer
-sudo apt-get install libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
+sudo apt-get install -y libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
