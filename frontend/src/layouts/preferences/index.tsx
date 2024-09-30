a54e9902b07a73b9f677fc07225926df51bac079
brandonhs <brandonnms@gmail.com>
2024-09-30 13:43:43 -0700

    Bump version and do not show settingscard until settings are loaded

--- frontend/src/layouts/preferences/index.tsx
+++ frontend/src/layouts/preferences/index.tsx
@@ -8,9 +8,9 @@ import TextFieldButton from "../../components/TextFieldButton";
 import { useSnackbar } from "notistack";
 
 const PreferencesLayout = () => {
-    const [host, setHost] = useState("192.168.2.1");
-    const [port, setPort] = useState(5600);
     const { enqueueSnackbar } = useSnackbar();
+    const [host, setHost] = useState(undefined);
+    const [port, setPort] = useState(undefined);
 
     useEffect(() => {
         getSettings().then((settings) => {
@@ -32,42 +32,46 @@ const PreferencesLayout = () => {
                 padding: "0 3em",
             }}
         >
-            <SettingsCard cardTitle='Stream'>
-                <TextField
-                    sx={styles.settingsButton.textInput}
-                    label='Default Stream Host'
-                    onChange={(e) => {
-                        setHost(e.target.value);
-                    }}
-                    value={host}
-                    variant='outlined'
-                    size='small'
-                />
-                <TextField
-                    sx={styles.settingsButton.textInput}
-                    label='Default Stream Port'
-                    onChange={(e) => setPort(parseInt(e.target.value))}
-                    value={port}
-                    variant='outlined'
-                    size='small'
-                    type='number'
-                    inputProps={{ min: 1024, max: 65535 }}
-                />
-                <Button
-                    sx={styles.settingsButton.button}
-                    variant='contained'
-                    onClick={() => {
-                        if (IP_REGEX.test(host))
-                            savePreferences({ default_stream: { host, port } });
-                        else
-                            enqueueSnackbar("Invalid IP Address", {
-                                variant: "error",
-                            });
-                    }}
-                >
-                    Save
-                </Button>
-            </SettingsCard>
+            {host === undefined || port === undefined || (
+                <SettingsCard cardTitle='Stream'>
+                    <TextField
+                        sx={styles.settingsButton.textInput}
+                        label='Default Stream Host'
+                        onChange={(e) => {
+                            setHost(e.target.value);
+                        }}
+                        value={host}
+                        variant='outlined'
+                        size='small'
+                    />
+                    <TextField
+                        sx={styles.settingsButton.textInput}
+                        label='Default Stream Port'
+                        onChange={(e) => setPort(parseInt(e.target.value))}
+                        value={port}
+                        variant='outlined'
+                        size='small'
+                        type='number'
+                        inputProps={{ min: 1024, max: 65535 }}
+                    />
+                    <Button
+                        sx={styles.settingsButton.button}
+                        variant='contained'
+                        onClick={() => {
+                            if (IP_REGEX.test(host))
+                                savePreferences({
+                                    default_stream: { host, port },
+                                });
+                            else
+                                enqueueSnackbar("Invalid IP Address", {
+                                    variant: "error",
+                                });
+                        }}
+                    >
+                        Save
+                    </Button>
+                </SettingsCard>
+            )}
         </Grid>
     );
 };
