ac53aa5a54f85fa4d93d1492d299b1738d89bb3b
brandonhs <brandonnms@gmail.com>
2024-10-01 20:54:59 -0700

    Make sure build files are not packaged

--- backend_py/clean.sh
+++ backend_py/clean.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Remove the build directory if it exists
-rm -rf build || true
+find . -type f -name "*.so" -exec rm {} +
 
 # Find and remove all __pycache__ directories
 find . -type d -name "__pycache__" -exec rm -rf {} +
 
 # Remove the device_settings.json file if it exists
 rm -f device_settings.json || true
+rm -f server_preferences.json || true
