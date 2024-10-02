94e79c24f5dc6bd8569d46aae7f8182074775f61
brandonhs <brandonnms@gmail.com>
2024-10-01 19:20:23 -0700

    Generalize clean.sh

--- backend_py/clean.sh
+++ backend_py/clean.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# Remove the build directory if it exists
 rm -rf build || true
-rm -rf src/__pycache__ || true
-rm -rf src/devices/__pycache__ || true
-rm -rf src/lights/__pycache__ || true
+
+# Find and remove all __pycache__ directories
+find . -type d -name "__pycache__" -exec rm -rf {} +
+
+# Remove the device_settings.json file if it exists
 rm -f device_settings.json || true
