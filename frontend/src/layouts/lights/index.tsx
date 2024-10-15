69728963389e6fb4c18d7682eb97d4678144075d
brandonhs <brandonnms@gmail.com>
2024-10-15 14:24:01 -0700

    Add more information to lights, showing when no lights are detected

--- frontend/src/layouts/lights/index.tsx
+++ frontend/src/layouts/lights/index.tsx
@@ -1,27 +1,40 @@
 import Grid from "@mui/material/Grid";
-import React, { useEffect, useState } from "react";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import React, { useContext, useEffect, useState } from "react";
 import LightCard from "./components/LightCard";
 import { LightDevice } from "./types";
 import LightContext from "../../contexts/LightContext";
 import { proxy, subscribe } from "valtio";
 import { useSnackbar } from "notistack";
 import { getLights, setIntensity } from "./api";
+import { styles } from "../../style";
+import WebsocketContext from "../../contexts/WebsocketContext";
 
 const LightsLayout = () => {
     const { enqueueSnackbar } = useSnackbar();
 
     const [lights, setLights] = useState([] as LightDevice[]);
 
+    const [hasRequestedLights, setHasRequestedLights] = useState(false);
+
+    let { connected } = useContext(WebsocketContext) as { connected: boolean };
+
     useEffect(() => {
-        getLights().then((lights) => {
-            if (lights.length === 0) {
-                enqueueSnackbar("No PWM Devices Detected", {
-                    variant: "warning",
-                });
-            }
-            setLights(lights);
-        });
-    }, []);
+        if (connected) {
+            getLights().then((lights) => {
+                if (lights.length === 0) {
+                    enqueueSnackbar("No PWM Devices Detected", {
+                        variant: "warning",
+                    });
+                }
+                setHasRequestedLights(true);
+                setLights(lights);
+            });
+        } else {
+            setLights([]);
+        }
+    }, [connected]);
 
     return (
         <>
@@ -34,19 +47,53 @@ const LightsLayout = () => {
                     justifyContent: "space-evenly",
                 }}
             >
-                {lights.map((lightValue, index) => {
-                    const light = proxy(lightValue);
+                {lights.length === 0 && hasRequestedLights && (
+                    <Grid
+                        container
+                        spacing={0}
+                        direction='column'
+                        alignItems='center'
+                        justifyContent='center'
+                        sx={{ height: "60vh" }}
+                    >
+                        <Grid item xs={3}>
+                            <Paper
+                                elevation={1}
+                                sx={{
+                                    padding: "50px",
+                                    textAlign: "center",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    borderRadius: "15px",
+                                    ...styles.card,
+                                }}
+                            >
+                                <Typography variant='h4' component='div'>
+                                    No Lights Connected
+                                </Typography>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                )}
+                {connected &&
+                    lights.map((lightValue, index) => {
+                        const light = proxy(lightValue);
 
-                    subscribe(light, () => {
-                        setIntensity(index, light.intensity);
-                    });
+                        subscribe(light, () => {
+                            setIntensity(index, light.intensity);
+                        });
 
-                    return (
-                        <LightContext.Provider key={index} value={{ light }}>
-                            <LightCard key={index} />
-                        </LightContext.Provider>
-                    );
-                })}
+                        return (
+                            <LightContext.Provider
+                                key={index}
+                                value={{ light }}
+                            >
+                                <LightCard key={index} />
+                            </LightContext.Provider>
+                        );
+                    })}
             </Grid>
         </>
     );
