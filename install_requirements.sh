ea288fe84ae567825a23f6d6c8b84cd4263044b7
brandonhs <brandonnms@gmail.com>
2024-10-22 16:16:44 -0700

    Fix gstremer installation issue

--- install_requirements.sh
+++ install_requirements.sh
@@ -89,9 +89,7 @@ sudo apt-get install python3 python3-venv -y
 sudo apt-get install libdbus-glib-1-dev libdbus-1-dev libpython3-dev -y
 
 echo "Installing GStreamer dependencies..."
-sudo apt-get install -y libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
-    gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base \
-    gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
+sudo apt-get install -y libglib2.0-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-libav
 
 # Attempt to install ttyd through apt. If it fails, download from GitHub
 echo "Installing ttyd..."
