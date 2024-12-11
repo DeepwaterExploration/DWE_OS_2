b73f436bb78aaa8e16e5e3fca234d11220b17c9d
brandonhs <brandonnms@gmail.com>
2024-12-11 15:26:30 -0800

    Add filter for self

--- frontend/src/layouts/cameras/components/DeviceLeader.tsx
+++ frontend/src/layouts/cameras/components/DeviceLeader.tsx
@@ -70,45 +70,51 @@ export const DeviceLeader: React.FC<DeviceLeaderProps> = (props) => {
                             >
                                 None
                             </MenuItem>
-                            {props.leaders.map((dev) => {
-                                return (
-                                    <MenuItem
-                                        key={dev.bus_info}
-                                        value={dev.bus_info}
-                                        onClick={() => {
-                                            popupState.close();
-                                            if (device.leader)
+                            {props.leaders
+                                .filter(
+                                    (dev) => dev.bus_info !== device.bus_info
+                                )
+                                .map((dev) => {
+                                    return (
+                                        <MenuItem
+                                            key={dev.bus_info}
+                                            value={dev.bus_info}
+                                            onClick={() => {
+                                                popupState.close();
+                                                if (device.leader)
+                                                    setFollowerUpdate(
+                                                        device.leader,
+                                                        undefined
+                                                    );
+                                                if (dev.follower) {
+                                                    // just ignore the request
+                                                    enqueueSnackbar(
+                                                        `Device: ${dev.bus_info} already has a follower.`,
+                                                        { variant: "error" }
+                                                    );
+                                                    return;
+                                                }
+                                                device.leader = dev.bus_info;
+                                                setLeader(
+                                                    dev.bus_info,
+                                                    device.bus_info
+                                                );
+                                                enableStreamUpdate(
+                                                    device.leader
+                                                );
                                                 setFollowerUpdate(
-                                                    device.leader,
-                                                    undefined
+                                                    dev.bus_info,
+                                                    device.bus_info
                                                 );
-                                            if (dev.follower) {
-                                                // just ignore the request
-                                                enqueueSnackbar(
-                                                    `Device: ${dev.bus_info} already has a follower.`,
-                                                    { variant: "error" }
-                                                );
-                                                return;
-                                            }
-                                            device.leader = dev.bus_info;
-                                            setLeader(
-                                                dev.bus_info,
-                                                device.bus_info
-                                            );
-                                            enableStreamUpdate(device.leader);
-                                            setFollowerUpdate(
-                                                dev.bus_info,
-                                                device.bus_info
-                                            );
-                                            // good global state
-                                        }}
-                                    >
-                                        {dev.nickname.length > 0
-                                            ? `${dev.nickname}: ${dev.bus_info}`
-                                            : dev.bus_info}
-                                    </MenuItem>
-                                );
-                            })}
+                                                // good global state
+                                            }}
+                                        >
+                                            {dev.nickname.length > 0
+                                                ? `${dev.nickname}: ${dev.bus_info}`
+                                                : dev.bus_info}
+                                        </MenuItem>
+                                    );
+                                })}
                         </TextField>
                     </div>
                 )}
